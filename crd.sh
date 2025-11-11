70eb16e606e58de3fcdd65d0902743a8fee51b3b
paskofa10 <mr.paskofa10@jugda.com>
2025-11-11 21:04:25 +0700

    Create crd.sh

---
+++ crd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt update
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt --fix-broken install -y
+sudo apt update -y && sudo apt upgrade -y
+
+sudo apt install xfce4 xfce4-goodies xrdp -y
+
+echo "startxfce4" > ~/.xsession
+sudo chown $(whoami):$(whoami) ~/.xsession
+
+sudo systemctl enable xrdp
+
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt install ./chrome-remote-desktop_current_amd64.deb -y
